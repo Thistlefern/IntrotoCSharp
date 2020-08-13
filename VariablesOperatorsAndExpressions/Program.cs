dbc9d88f21ac91e3eb715327076628a8e05a51fa
Thistlefern <jlderosier73@yahoo.com>
2020-08-13 11:33:43 -0700

    Add tutorial for VARIABLES, Operators, and Expressions lesson

---
+++ VariablesOperatorsAndExpressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VariablesOperatorsAndExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing 1 2 3");
+            Console.ReadLine();
+        }
+    }
+}
