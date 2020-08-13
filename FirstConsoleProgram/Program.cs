17dbbadd7a98368dce8df188b13a1c8a71322ce0
Josie DeRosier <jlderosier73@yahoo.com>
2020-08-12 17:38:10 -0700

    Add first files

---
+++ FirstConsoleProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstConsoleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Green is my favorite color, and my favorite fruit is peaches.");
+            Console.ReadLine();
+        }
+    }
+}
