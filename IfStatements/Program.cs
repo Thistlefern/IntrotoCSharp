1846986736da5cbe31a6ad0dcea02bb63d574e34
Thistlefern <jlderosier73@yahoo.com>
2020-08-13 16:47:31 -0700

    If Statement tutorial completed

---
+++ IfStatements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int score = 0;
+            int combo = 1;
+            int pointValue = 100;
+            bool gameStarted = false;
+            if (score == 0)
+            {
+                gameStarted = true;
+                if (gameStarted)
+                {
+                    score = combo * pointValue;
+                    combo++;
+                    Console.WriteLine("You got a combo! Your new score is: " + score);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
