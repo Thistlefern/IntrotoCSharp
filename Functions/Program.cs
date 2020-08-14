702a84a3ac4f65b118bd21a0ae0c0b5b843f7951
Thistlefern <jlderosier73@yahoo.com>
2020-08-14 10:50:39 -0700

    Add completed functions tutorial

---
+++ Functions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static int numberOne = 10;
+        static int numberTwo = 200;
+        static void Start()
+        {
+            int biggestNumber = ReturnBiggestNumber(numberOne, numberTwo);
+            Console.WriteLine("Of " + numberOne + " and " + numberTwo + " the biggest number is " + biggestNumber);
+           
+            Console.ReadKey();
+        }
+        static int ReturnBiggestNumber(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+        static void Main()
+        {
+            Start();
+        }
+    }
+}
+
---
+++ IfStatementsExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IfStatementsExercise
+{
+    class Program
+    { // Review Questions
+      // 1. An if statement is a conditional statement in which one or more pieces of criteria are checked to be true, before enacting code that corresponds to whether the criteria was true or otherwise.
+      // 2. If the condition is true in an if statement, then the corresponding code is initiated.
+      // 3. An else statement takes a previous if statement, and gives the code to initiate in the case that the if statement is false.
+      // 4. An else if statement gives additional criteria to a previous if statement, but prior to a final else statement.
+      // 5. 
+      // 6. 
+      // 7. 
+      // 8. 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
