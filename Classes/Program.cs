3c8d391894b4a9f2d8e668aa56d479e11b6ba865
Thistlefern <jlderosier73@yahoo.com>
2020-08-20 14:13:26 -0700

    Added completed Classes Exercise after learning about arrays.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,6 +6,8 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            Team dogCorp = new Team();
+
             Employee ike = new Employee();
             ike.firstName = "Ike";
             ike.lastName = "DeRosier";
@@ -24,18 +26,25 @@ namespace Classes
             Employee leader = new Employee();
             leader.firstName = "Josie";
             leader.lastName = "DeRosier";
-            leader.yearsOfExperience = 24;
+            leader.yearsOfExperience = 6;
+
+            dogCorp.employeeList[0] = ike;
+            dogCorp.employeeList[1] = tike;
+            dogCorp.employeeList[2] = pike;
+            dogCorp.employeeList[3] = leader;
 
             ike.PrintBio();
             tike.PrintBio();
             pike.PrintBio();
             leader.PrintBio();
 
-            float average = (ike.yearsOfExperience+tike.yearsOfExperience+pike.yearsOfExperience)/3;
-            float averageWithLead = ((average * 3) + leader.yearsOfExperience) / 4;
+            dogCorp.averageExperience();
 
-            Console.WriteLine($"\nNot including the leader, the average years of service among employees is {average}.");
-            Console.WriteLine($"When you include the leader, the average number of years of service becomes {averageWithLead}.\n");
+            //float average = (ike.yearsOfExperience+tike.yearsOfExperience+pike.yearsOfExperience)/3;
+            //float averageWithLead = ((average * 3) + leader.yearsOfExperience) / 4;
+
+            //Console.WriteLine($"\nNot including the leader, the average years of service among employees is {average}.");
+            //Console.WriteLine($"When you include the leader, the average number of years of service becomes {averageWithLead}.\n");
         }
         
     }
--- Classes/Team.cs
+++ Classes/Team.cs
@@ -1,13 +1,33 @@
-﻿using System;
+﻿using Classes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Classes
 {
+    
     class Team
     {
-        //I created this class, and then realized that the tutorial asked for an array.
-        //We haven't learned arrays yet, so I'm skipping this bit.
-        //If I misunderstood and need to do more here, let me know!
+
+        public Employee[] employeeList = new Employee[5];
+
+        public void averageExperience()
+        {
+            float totalYears = 0;
+            int employeeCount = 0;
+
+            foreach (Employee cMember in employeeList)
+            {
+                if (cMember != null)
+                {
+                    totalYears += cMember.yearsOfExperience;
+                    employeeCount++;
+                }
+            }
+            Console.WriteLine($"The average experience among employees is {totalYears / employeeCount} years.");
+
+        }
+
+
     }
 }
