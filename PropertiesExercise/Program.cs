e2450acd518f28d8c3586f16f9c42ef142db4d33
Thistlefern <jlderosier73@yahoo.com>
2020-09-02 16:24:33 -0700

    Pushing properties exercise

---
+++ PropertiesExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PropertiesExercise
+{
+    class Money
+    {
+        public double aud;
+        public double usd;
+
+        public double AUD
+        {
+            get
+            {
+                return aud;
+            }
+        }
+        
+        public double USD
+        {
+            get
+            {
+                return usd;
+            }
+            set
+            {
+                value = aud * 0.8;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Money wallet = new Money();
+            wallet.aud = 10;
+            Console.WriteLine($"AUD: {wallet.aud}");
+            Console.WriteLine($"USD: {wallet.usd}");
+        }
+    }
+}
